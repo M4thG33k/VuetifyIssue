aec24a275ea552701c88c37c08f647021f530a58
M4thG33k <pepethemathgeek@gmail.com>
2022-06-07 11:47:29 -0500

    Commits issue with VTextField

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <InputWithAppend/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import InputWithAppend from "./components/InputWithAppend.vue";
+
+export default {
+  name: 'App',
+
+  components: {
+    InputWithAppend,
+  },
+}
+</script>
---
+++ src/components/InputWithAppend.vue
@@ -0,0 +1,19 @@
+<template>
+  <VCard :flat="true">
+    <VTextField label="Anything">
+      <template #appendInner>
+        Edit me
+      </template>
+    </VTextField>
+  </VCard>
+</template>
+
+<script>
+export default {
+  name: "InputWithAppend",
+}
+</script>
+
+<style scoped>
+
+</style>
